dfb4b8f24f073649b55d72550f81df9163229e04
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-05-15 21:47:12 +0300

    Update numbers.js

---
+++ js/numbers.js
@@ -0,0 +1,18 @@
+let elementWidth = "50 px";
+const result = Number.parseInt(elementWidth);
+
+console.log(result);
+//  50 - запис буде такий в результаті
+
+
+
+// let elementWidth = "50 px";
+
+// elementWidth = Number.parseInt(elementWidth);
+// console.log("elementWidth:",elementWidth);
+// elementWidth: 50 - запис буде такий в результаті
+let elementHeight = "288.74px";
+elementHeight = Number.parseFloat(elementHeight);
+
+console.log("elementHeight:", elementHeight);
+// elementHeith: 288.74 - запис буде такий в результаті
