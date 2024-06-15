c548c88f8a27e85b74ef3382ed4dd03c9497d880
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-06-15 15:41:05 +0300

    add strings.js and update strings.js

---
+++ js/strings.js
@@ -0,0 +1,13 @@
+// важливо памёятати.!!!
+// приклад message.length-звернення до свойства(характеристики)значеня
+// приклад messagelength()-виклик метода (якогось)(із пузатими дужками - це виклик методу) )
+// const message = 'в цій строці 24 символів';
+// console.log(message.length);
+
+// ===========================
+// конкотенація строк. або сшивання з допомогою оператора +( плюс).
+// плюс це оператор з боків від нього операнди
+const firstName = 'Chelsy';
+const lastName = 'Emerald';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
