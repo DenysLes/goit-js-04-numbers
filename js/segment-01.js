b1f8b25a6868d3f7d953bbde9145ec89dde82144
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-06-25 16:26:21 +0300

    update logigal-operatiors.js/ add segment-01.js

---
+++ js/segment-01.js
@@ -0,0 +1,11 @@
+// Задачі
+const x1 = 10;
+const x2 = 30;
+const number = 15;
+
+console.log('Число $(number) попадає у відрізок до $(x1)?',number < x1);
+console.log('Число $(number) попадає у відрізок після $(x2)?', number > x2);
+
+const res = number > x1 && number < x2;
+
+console.log('Число $(number) попадає у відрізок від $(x1) до $(x2)?' ,res); 
