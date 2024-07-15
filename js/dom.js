13e58db147125045d4e80e27b38b389903e8ddb6
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-07-15 17:23:22 +0300

    update html and dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,21 @@
 const btnAdd = document.querySelector(`button[data-add]`);
 const valueInput = document.querySelector(`input[data-value]`);
-const outputEl = document.querySelector(`js-output`);
+const outputEl = document.querySelector(`.js-output span`);
+
+let total = 0;
+
+console.log(btnAdd);
+btnAdd.addEventListener(`click`, function () {
+    console.log(`click hehehehe`);
+
+const value = Number(valueInput.value);
+
+console.log(value);
+
+total += value;
+outputEl.textContent = total;
+valueInput.value = '';
+});
+
+
 
