06272bc8016e2f6ac4b3713ab19fb2ba0601c517
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-07-20 16:22:00 +0300

    Update even.js

--- js/even.js
+++ js/even.js
@@ -1,17 +1,27 @@
 // Напиши скрипт который посчытивает все чётные чысла в массиве
-const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
 let total = 0;
 
-for (let i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
+// for (let i = 0; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
 
-    if (numbers[i] % 2 === 0) {
-        console.log(`чётное:`,numbers[i]);
-   total += numbers[i];
+//     if (numbers[i] % 2 === 0) {
+//         console.log(`чётное:`,numbers[i]);
+//    total += numbers[i];
+//     }
+// }
+for (const number of numbers) {
+// console.log(number);
+
+if (number % 2 !== 0) {
+console.log(`не Чётное! Эту итерацию нужно пропустить ! - `,number);
+continue;
     }
-}
-
-   console.log(`Total:`, total);
+   
+    console.log(`${number} - Чётное !!!`);
+    total += number;
+ }
+console.log(`Total:`, total);
 
 
  
