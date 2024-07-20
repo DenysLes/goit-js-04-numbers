2f9784610dab7b1e852c5e32aad31e5d685269e8
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-07-20 15:48:38 +0300

    Update even.js

--- js/even.js
+++ js/even.js
@@ -3,14 +3,12 @@ const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
 let total = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
-    console.log(i);
+    console.log(numbers[i]);
 
-    if (i % 2 !== 0) {
-        console.log(`не чётное:`,i);
-        continue;
-        }
-    // total += i;
-    console.log(`чётное число:`, i);
+    if (numbers[i] % 2 === 0) {
+        console.log(`чётное:`,numbers[i]);
+   total += numbers[i];
+    }
 }
 
    console.log(`Total:`, total);
