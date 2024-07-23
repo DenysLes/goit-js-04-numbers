09fc64d0922650c84f0f96183b5899248fb3d6d3
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-07-23 18:52:00 +0300

    update cards.js / add basi2(function).js

---
+++ js/basic2(function).js
@@ -0,0 +1,27 @@
+// const add = function (x, y) {
+//     console.log(x);
+//     console.log(y);
+    
+//     const result = x + y;
+//     console.log(`x + y =`, result);
+//     console.log(`add - выполняется функция`);
+// }
+
+// add(5,10)
+// add(20,30)
+// add(30, 50)
+
+const add = function (x, y) {
+    console.log(x);
+    console.log(y);
+    console.log(`add - выполняется функция`);
+    
+return x + y;
+}
+
+const r1 = add(5, 10);
+console.log(`r1 :` ,r1);
+const r2 = add(20, 30);
+console.log(`r2 :` ,r2);
+const r3 = add(30, 50);
+console.log(`r3 :` ,r3);
--- js/cards.js
+++ js/cards.js
@@ -5,22 +5,55 @@ const cards = [
     'Карточка-4',
     'Карточка-5'
 ];
+
 console.table(cards);
 
 // Удаление по индексу indexOf
 // const cardToRemove = 'Карточка-3';
 // const index = cards.indexOf(cardToRemove);
+// console.log(cardToRemove);
 // console.log(index);
-// cards.splice(index, 1);
 
-// console.table(cards);
+// cards.splice(index, 3);
+// console.table(cards); //Останутся 4 и 5
+
+// cards.splice(0, 3);
+// console.table(cards);//Останутся 1 и 2
 // Добавление по индексу
-const cardToInsert = 'Карточка-6';
-const index = 3;
+// const cardToInsert = 'Карточка-6';
+// const index = 3;
 
-cards.splice(1, 0, 7, 8, 9);
-console.table(cards);
+// cards.splice(index, 0, cardToInsert);
+// console.table(cards)
+
+//splice(position, 0, new_element_1, new_element_2, ...)
+// cards.splice(1, 0, 7, 8, 9);//splice(position, 0, new_element_1, new_element_2, ...)
+// Аргумент position вказує початкову позицію в масиві, куди будуть вставлені нові елементи.
+// Другий аргумент - це нуль, він говорить методу не видаляти елементи в місці додавання нових.
+// Третій, четвертий і всі наступні аргументи - це нові елементи, які додаються в масив
+// console.table(cards);
+
+// const colors = ["red", "green", "blue"];
+
+// colors.splice(2, 0, "purple");
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+
+// Можна додати довільну кількість елементів, передавши четвертий, п'ятий аргумент тощо.
+
+
+
+// const colors2 = ["red", "green", "blue"];
+
+// colors2.splice(1, 0, "yellow", "pink");
+// console.log(colors2); // ["red", "yellow", "pink", "green", "blue"]
+
 
 
 // Обнов вление по индексу
-const cardToUpdate = 'Карточка-1';
+const cardToUpdate = 'Карточка-4';
+const index = cards.indexOf(cardToUpdate) ;
+
+cards.splice(index, 1, `Обновлённая Карточка-4`);
+console.table(cards);
+
