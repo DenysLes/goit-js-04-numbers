6ea4485067cba6447394215aff2dc2b1165ceb92
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-07-27 21:29:53 +0300

    add slugify.js

--- js/change-case.js
+++ js/change-case.js
@@ -2,14 +2,32 @@
 -каждый символ на противоположный
 -Например,если строка`JavaScript`,то должно получится `jAVAsCRIPT`
 */
-const string ='QwErtZXcV';
-const letters = string.split('');
-let  invertedString  = '';
+// const string ='JavaScript';
+// const letters = string.split('');
+// let  invertedString  = '';
 
-for (const letter of letters) {
-    const isEqual = letter === letter.toLowerCase();
+// for (const letter of letters) {
+//     const isInLowerCase = letter === letter.toLowerCase();
 
+// invertedString += isInLowerCase ? letter.toUpperCase() : letter.toLowerCase();
+// }
+// console.log(`invertedString:`, invertedString);
 
-invertedString += isEqual ? letter.toUpperCase() : letter.toLowerCase();
+const changeCase = function (string) {
+    const letters = string.split('');
+    let invertedString = '';
+
+    for (const letter of letters) {
+        const isInLowerCase = letter === letter.toLowerCase();
+
+        invertedString += isInLowerCase
+            ? letter.toUpperCase()
+            : letter.toLowerCase();
+    }
+    return invertedString;
+
 }
-console.log(`invertedString:` , invertedString);
+
+console.log(changeCase(`qweRTY`));
+console.log(changeCase(`ManGo`));
+console.log(changeCase(`AjAx`));
---
+++ js/slugify.js
@@ -0,0 +1,27 @@
+// Напиши функцию slugifi(string) которая получает строку и возвращает URL- slug
+// Строка состоит только из букв и пробелов
+
+// const title = `Top 10 benefits of React framework`;
+
+
+// const normalizedTitle = title.toLowerCase();
+// const words = normalizedTitle.splise(` `);
+// const slug = words.join(`-`);
+
+// const slug1 = title.toLowerCase().split(` `).join(`-`);
+
+// console.log(slug1);
+
+const slugify = function (string) {
+
+    const normalizedTitle = string.toLowerCase();
+    const words = normalizedTitle.split(' ');
+    const slug = words.join(`-`);
+    
+    return slug;
+}
+
+
+console.log(slugify(`Top 10 benefits of React framework`));
+console.log(slugify(`Azure Static Web Apps are Awesome`));
+console.log(slugify(`Technical writing tips for non-native English speakers`));
