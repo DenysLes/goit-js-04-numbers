db0b990d8cf81dbaf2fe4568219d6dbac87f65ea
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-09-09 17:43:17 +0300

    update array-sum.js and exp.js

--- js/array-sum.js
+++ js/array-sum.js
@@ -3,7 +3,7 @@
 const array1 = [5, 10, 15, 20];
 const array2 = [10, 20, 30];
 
-let total = 0;
+// let total = 0;
 
 // for (let i = 0; i < array1.length; i += 1){
 //     total += array1[i];
--- js/exp.js
+++ js/exp.js
@@ -2,17 +2,19 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newArray = [];
 
 
-// array.forEach(elem => console.log(array));
+array.forEach(elem => console.log(array));
 
 // array.forEach((elem) => {
 //     console.log(array)
 // });
 
-// // for (let item of array) {
-// // console.log(array)
+// for (let item of array) {
+// console.log(array)
 // }
 
-array.forEach((elem) => {
-newArray.push((elem += elem))
-});
-console.log(newArray)
+// array.forEach((elem) => {
+// newArray.push((elem += elem))
+// });
+// console.log(newArray)
+
+
