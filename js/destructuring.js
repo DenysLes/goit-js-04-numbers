e32b7b29b87dcfc18d6f14bf21c398d599c253d8
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-03 17:37:17 +0200

    Update destructuring.js

--- js/destructuring.js
+++ js/destructuring.js
@@ -5,9 +5,10 @@ const atTheOldToad = {
     { name: "Stone skin", price: 520 },
     ],
     // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
+    getPotions() {
+       console.log(typeof atTheOldToad);
+      return this.potions;
+       },
   addPotion(newPotion) {
     if (this.potions.includes(newPotion)) {
       return `Error! Potion ${newPotion} is already in your inventory!`;
@@ -23,7 +24,7 @@ const atTheOldToad = {
     }
 
       this.potions.splice(potionIndex, 1);       
-
+     
   },
   updatePotionName(oldName, newName) {
     const potionIndex = this.potions.indexOf(oldName);
@@ -31,11 +32,21 @@ const atTheOldToad = {
     if (potionIndex === -1) {
       return `Potion ${oldName} is not in inventory!`;
     }
-console.log(atTheOldToad);
+
     this.potions.splice(potionIndex, 1, newName);
     },
  
   // Change code above this line
 };
-
-atTheOldToad.removePotion("Dragon breath")
+atTheOldToad.getPotions();
+atTheOldToad.addPotion({ name: "Invisibility", price: 620 });
+atTheOldToad.addPotion({ name: "Power potion", price: 270 });
+atTheOldToad.addPotion({ name: "Dragon breath", price: 700 });
+atTheOldToad.addPotion({ name: "Stone skin", price: 240 });
+atTheOldToad.addPotion({ name: "Dragon breath", price: 700 });
+atTheOldToad.addPotion({ name: "Stone skin", price: 240 });
+atTheOldToad.removePotion("Dragon breath");
+atTheOldToad.removePotion("Speed potion");
+atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion");
+// atTheOldToad.removePotion("Dragon breath");
