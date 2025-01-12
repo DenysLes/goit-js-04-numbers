90c26a31452f1b4ead5cc6ec8e268fed50eddc4b
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-12 16:55:45 +0200

    update

--- js/calculate-total-price.js
+++ js/calculate-total-price.js
@@ -2,13 +2,13 @@
 - Напиши функцию с calculate total price
 - которая принимает массив цен(чисел) и возвращает их сумму
 */
-// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
-// let total = 0;
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+let total = 0;
 
-// for (const value of cart) {
-//     total += value;
-// }
-// console.log(`Total =`, total);
+for (const value of cart) {
+    total += value;
+}
+console.log(`Total =`, total);
 
 const calculateTotalPrice = function (items) {
     console.log(`items внутри функции `,items);
--- js/dom.js
+++ js/dom.js
@@ -1,23 +1,23 @@
-// const btnAdd = document.querySelector(`button[data-add]`);
-// const valueInput = document.querySelector(`input[data-value]`);
-// const outputEl = document.querySelector(`.js-output span`);
-// const resetBtn = document.querySelector(`button[data-reset]`);
-// let total = 0;
+const btnAdd = document.querySelector(`button[data-add]`);
+const valueInput = document.querySelector(`input[data-value]`);
+const outputEl = document.querySelector(`.js-output span`);
+const resetBtn = document.querySelector(`button[data-reset]`);
+let total = 0;
 
-// console.log(btnAdd);
-// btnAdd.addEventListener(`click`, function () {
-//     console.log(`click hehehehe`);
+console.log(btnAdd);
+btnAdd.addEventListener(`click`, function () {
+    console.log(`click hehehehe`);
 
-// const value = Number(valueInput.value);
+const value = Number(valueInput.value);
 
-// console.log(value);
+console.log(value);
 
-// total += value;
-// outputEl.textContent = total;
-// valueInput.value = '';
-// });
+total += value;
+outputEl.textContent = total;
+valueInput.value = '';
+});
 
-// resetBtn.addEventListener(`click`, function () {
-//   outputEl.textContent = 0;
-// });
+resetBtn.addEventListener(`click`, function () {
+  outputEl.textContent = 0;
+});
 
